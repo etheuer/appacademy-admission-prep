ac679d8880c15dc97d0d7110167a4d887f5d4e26
Erik Theuer <erik.theuer@gmail.com>
2015-04-02 12:26:53 -0300

    Added official solution

--- 12-dasherize_number.rb
+++ 12-dasherize_number.rb
@@ -57,8 +57,28 @@ end
 # Official solution:
 
 
+def dasherize_number(num)
+  num_s = num.to_s
 
+  result = ""
 
+  idx = 0
+  while idx < num_s.length
+    digit = num_s[idx].to_i
+
+    if (idx > 0)
+      prev_digit = num_s[idx - 1].to_i
+      if (prev_digit % 2 == 1) || (digit % 2 == 1)
+        result += "-"
+      end
+    end
+    result += num_s[idx]
+
+    idx += 1
+  end
+
+  return result
+end
 
 
 
